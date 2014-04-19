1ea3dced76f557ceb99655347d36a62634249859
Duncan Butler <duncan.butler1@btinternet.com>
2014-04-19 23:15:28 +0100

    added base bdd test class

---
+++ CheckoutBasket.Specifications/BddTesting.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace CheckoutBasket.Specifications
+{
+    [TestFixture]
+    public abstract class BddBase
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Given();
+
+            When();
+        }
+
+        protected abstract void Given();
+
+        protected abstract void When();
+    }
+}
