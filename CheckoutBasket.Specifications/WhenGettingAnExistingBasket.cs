22a65c03611a11f01aed19ad9b65bb7fbb3730e1
Duncan Butler <duncan.butler1@btinternet.com>
2014-05-24 14:21:10 +0100

    failing get basket test

---
+++ CheckoutBasket.Specifications/WhenGettingAnExistingBasket.cs
@@ -0,0 +1,39 @@
+﻿using CheckoutBasket.API.Domain;
+using Nancy;
+using Nancy.Testing;
+using NUnit.Framework;
+using Shouldly;
+
+namespace CheckoutBasket.Specifications
+{
+    public class WhenGettingAnExistingBasket : BddBase
+    {
+        private Browser _browser;
+        private Basket _basket;
+        private string _basketId;
+
+        protected override void Given()
+        {
+            var bootstrapper = new DefaultNancyBootstrapper();
+            _browser = new Browser(bootstrapper);
+
+            var response = _browser.Get("/basket");
+
+            var basket = response.Body.DeserializeJson<Basket>();
+            _basketId = basket.Id;
+        }
+
+        protected override void When()
+        {
+            var response = _browser.Get(string.Format("/basket/{0}",_basketId));
+
+            _basket = response.Body.DeserializeJson<Basket>();
+        }
+
+        [Test]
+        public void Specification()
+        {
+            _basket.Id.ShouldBe(_basketId);            
+        }
+    }
+}
